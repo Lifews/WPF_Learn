9b285cd0f2786e53a271d81f3c1088bbbfebed46
suki T <tsuki_tsuki1993@163.com>
2025-07-16 11:50:27 +0800

    一个简单示例，利用Toolkit的ObservableValidator做输入校验，还做了界面本地化，可切换语言。

---
+++ Toolkit_ObservableValidator/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Data;
+using System.Windows;
+
+namespace Toolkit_ObservableValidator.Converters;
+
+internal class StringToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        return string.IsNullOrEmpty(value as string) ? System.Windows.Visibility.Collapsed : System.Windows.Visibility.Visible;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        // Not needed
+        throw new NotImplementedException();
+    }
+}
