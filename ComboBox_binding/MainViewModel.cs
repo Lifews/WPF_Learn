ce816daa478db0989f551174e1e5a2e17ff5e71c
Lifews <tsuki_tsuki1993@163.com>
2025-07-22 00:41:42 +0800

    ComboBox的一种绑定方法，不太方便，有待优化

---
+++ ComboBox_binding/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace ComboBox_binding;
+
+public partial class MainViewModel : ObservableObject
+{
+    // 使用源生成器简化属性
+    [ObservableProperty]
+    private UserRole _selectedRole = UserRole.RegularUser;
+
+    // 使用只读属性初始化枚举选项
+    public ObservableCollection<KeyValuePair<UserRole, string>> Roles { get; }
+        = new(Enum.GetValues<UserRole>()
+            .Select(e => new KeyValuePair<UserRole, string>(
+                e,
+                e.GetAttribute<DescriptionAttribute>()?.Description ?? e.ToString()
+            )));
+}
+
+// 扩展方法获取特性
+public static class EnumExtensions
+{
+    public static T? GetAttribute<T>(this Enum value) where T : Attribute
+    {
+        var field = value.GetType().GetField(value.ToString());
+        return field?.GetCustomAttributes(typeof(T), false)?.FirstOrDefault() as T;
+    }
+}
