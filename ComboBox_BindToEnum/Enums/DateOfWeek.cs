5193b5f1f5aad9172e41e6fabb28e513237857a5
Lifews <tsuki_tsuki1993@163.com>
2025-07-22 23:18:54 +0800

    WPF中如何将ComboBox绑定到后台的枚举类型从而生成选项

---
+++ ComboBox_BindToEnum/Enums/DateOfWeek.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace ComboBox_BindToEnum;
+
+public enum DateOfWeek
+{
+    //微软官方定义有DayOfWeek枚举,此处为了演示，使用自定义枚举
+
+    [Description("星期日")]
+    Sunday,
+
+    [Description("星期一")]
+    Monday,
+
+    [Description("星期二")]
+    Tuesday,
+
+    [Description("星期三")]
+    Wednesday,
+
+    [Description("星期四")]
+    Thursday,
+
+    [Description("星期五")]
+    Friday,
+
+    [Description("星期六")]
+    Saturday
+}
---
+++ ComboBox_BindToEnum/ExtensionsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ComboBox_binding;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComboBox_BindToEnum;
+
+public partial class ExtensionsViewModel : ObservableObject
+{
+    [ObservableProperty]
+    DateOfWeek dateOfWeek;
+}
